ef2e35255f98a5159b4d774fc4b94b5f1c4072c9
VacaSama <klc_moore@outlook.com>
2025-08-22 17:23:42 -0400

    Connection String made in the eCommerceDatabaseContext.cs within the Data Folder. no test yet to determine if the connection is working, but sample data has been inserted.

--- eCommerce/Data/eCommerceDatabaseContext.cs
+++ eCommerce/Data/eCommerceDatabaseContext.cs
@@ -1,6 +1,15 @@
-﻿namespace eCommerce.Data;
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace eCommerce.Data;
 
 public class eCommerceDatabaseContext
 {
+    // connection string for the database 
+    public string connectionString = "Data Source=localhost;Initial Catalog=MilkmanFarmsDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
+    public eCommerceDatabaseContext()
+    {
+        // Initialize the database context here
+        // This could include setting up a connection string, configuring options, etc.
+    }
 }
