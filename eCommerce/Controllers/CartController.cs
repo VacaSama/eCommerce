9da696528b2e7a9ed642a4dfc9b6977c6211dc82
VacaSama <klc_moore@outlook.com>
2025-08-27 17:48:11 -0400

    Methods in the CartController and ProductController have been updated so that database context can go within them.
    Product/Index is being updated for cohesive functionality using database sample data. Product/Index Name,Price and Category need to be updated on the third card- Chocolate Delight then the branch can be called for a pull request.

--- eCommerce/Controllers/CartController.cs
+++ eCommerce/Controllers/CartController.cs
@@ -1,27 +1,48 @@
 ﻿using eCommerce.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Azure.Core;
 
-namespace eCommerce.Controllers
+namespace eCommerce.Controllers;
+
+public class CartController : Controller
 {
-    public class CartController : Controller
+    private readonly eCommerceDatabaseContext _context;
+
+    //// constructor 
+    public CartController(eCommerceDatabaseContext context)
     {
-        //private readonly eCommerceDatabaseContext _context;
+        _context = context;
+    }
 
-        //// constructor 
-        //public CartController(eCommerceDatabaseContext context)
-        //{
-        //    _context = context;
-        //}
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        //public IActionResult Add(int id)
-        //{
-        //    var product = _context.Products.FirstOrDefault(x => x.Id == id);
-        //}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    /// <summary>
+    /// This action will allow the user to add something to their cart 
+    /// and pushes the product id to the cart 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpPost]
+    public IActionResult AddCart(int id)
+    {
+        return View();
+    }
+
+
+    /// <summary>
+    /// This action/method will allow the user to remove something from their 
+    /// cart. If the cart is empty a message will appear.
+    /// </summary>
+    /// <returns></returns>
+    [HttpDelete]
+
+    public IActionResult RemoveCart()
+    {
+        return View();
     }
 }
