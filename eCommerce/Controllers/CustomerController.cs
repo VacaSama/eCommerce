03370d36c12774c4d896ab4139c5001c0661680a
VacaSama <klc_moore@outlook.com>
2025-08-30 21:45:55 -0400

    Register/login pages issue18 (#23)
    * CustomerController made to keep track of customer login and resgistration of new customers. The Customers Table will be utilized here.
    
    RegistrationViewModel has been made so that hte register view can reference from it, in view.
    
    Added annotations to the RegistrationViewModels properties that have been copied from the Customer Class, other properties will be used later to update the customer profile/ send orders their way.
    
    `Layout.cshtml` - the navbar has been updated so that the users can reach the register view.
    
    * Migrations have been made, updates to the database have occured to go with the RegistrationViewModel class
    
    Final Test then moving on to the Login information.
    
    LoginViewModel class has been made requiring customer to enter email and password in order to login.
    
    Login View/Class has been made.
    
    * Migrations made to update the password storing in database.
    
    CustomerController updated with login and register functionality, page return is not quite 100% --
    
    Customer Class has been altered and contains other related classes to keep the code clean and clear. RegistrationView Model and LoginViewModel
    
    asp validation placed within theLogin view. so that correct database information can be send and stored.
    Layout.cshtml has been updated so that the login link can also be in the nav bar.

---
+++ eCommerce/Controllers/CustomerController.cs
@@ -0,0 +1,72 @@
+﻿using eCommerce.Data;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Controllers;
+
+public class CustomerController : Controller
+{
+
+    private readonly eCommerceDatabaseContext _context;
+
+
+    // constructor
+    public CustomerController(eCommerceDatabaseContext context)
+    {
+        _context = context;
+    }
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegistrationViewModel reg)
+    {
+        if (ModelState.IsValid)
+        {
+            Customer newCustomer = new()
+            {
+                FirstName = reg.FirstName,
+                LastName = reg.LastName,
+                Email = reg.Email,
+                Password = reg.Password,
+                ConfirmPassword = reg.ConfirmPassword,
+                Phone = reg.Phone,
+                StreetAddress = reg.StreetAddress,
+                City = reg.City,
+                State = reg.State
+            };
+            _context.Customers.Add(newCustomer);
+            await _context.SaveChangesAsync();
+
+            //placeholer later we an redirect to the login page
+            return RedirectToAction("Index", "Home");
+        }
+        return View(reg);
+    }
+
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel login)
+    {
+        if (ModelState.IsValid && login != null)
+        {
+            // check if a customer email already matches to one in the database?
+
+            Customer? loggedInCustomer = await _context.Customers.Where(c => c.Email == login.Email 
+                              && c.Password == login.Password).SingleOrDefaultAsync();
+        }
+
+        return View(login);
+    }
+
+}
--- eCommerce/Models/Customer.cs
+++ eCommerce/Models/Customer.cs
@@ -1,19 +1,116 @@
-﻿namespace eCommerce.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.Models;
+
+public class Customer
 {
-    public class Customer
-    {
-        public int CustomerId { get; set; }
-        public required string FirstName { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int CustomerId { get; set; }
 
-        public required string LastName { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public required string FirstName { get; set; }
 
-        public required string Email { get; set; }
-        public string? Phone { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public required string LastName { get; set; }
 
-        public required string StreetAddress { get; set; } 
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataType(DataType.EmailAddress)]
+    public required string Email { get; set; }
 
-        public required string City { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// 
+    [StringLength(50, MinimumLength = 8)]
 
-        public required string State { get; set; }
-    }
+    [DataType(DataType.Password)]
+    public required string Password { get; set; }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+    [DataType(DataType.Password)]
+    public required string ConfirmPassword { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string? Phone { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+
+    public required string StreetAddress { get; set; } 
+
+    /// <summary>
+    /// 
+    /// </summary>
+
+    public required string City { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public required string State { get; set; }
 }
+
+
+/// <summary>
+/// 
+/// </summary>
+public class RegistrationViewModel
+{
+
+    public required string FirstName { get; set; }
+
+    public required string LastName { get; set; }
+
+
+    [DataType(DataType.EmailAddress)]
+    public required string Email { get; set; }
+
+
+    [StringLength(50, MinimumLength = 8)]
+
+    [DataType(DataType.Password)]
+    public required string Password { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+    [DataType(DataType.Password)]
+    public required string ConfirmPassword { get; set; }
+
+    public string? Phone { get; set; }
+
+    public required string StreetAddress { get; set; }
+
+    public required string City { get; set; }
+
+    public required string State { get; set; }
+}
+
+public class LoginViewModel
+{
+
+    [DataType(DataType.EmailAddress)]
+    public required string  Email { get; set;}
+
+
+    [DataType(DataType.Password)]
+    public required string  Password {get; set;} 
+}
+  
