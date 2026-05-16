1f5190bafae48511c610fb34db9c1669566d8643
VacaSama <klc_moore@outlook.com>
2026-05-16 15:41:11 -0400

    Adding a SeedData class, this will store all necessary entities for the database.
    Since this was one of my newer projects and I am going back to fix it, data annotations, summaries and model updates are also being changed little by little.
    
    Some products have been added but have not been implemented in the database as of yet.

---
+++ eCommerce/Data/SeedData.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Data
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new eCommerceDatabaseContext(
+                serviceProvider.GetRequiredService<DbContextOptions<eCommerceDatabaseContext>>()))
+            {
+                // Look for any products.
+                if (context.Products.Any())
+                {
+                    return;   // DB has been seeded
+                }
+                context.Products.AddRange(
+                    new Models.Product
+                    {
+                        Name = "Original Milkman",
+                        Price = 7.99m,
+                        Category = "Dairy"
+                    },
+                    new Models.Product
+                    {
+                        Name = "Little Chubbs Chocolate Delight",
+                        Price = 7.99m,
+                        Category = "Dairy"
+                    },
+                    new Models.Product
+                    {
+                        Name = "Aunt May Berry Pie",
+                        Price = 8.99m,
+                        Category = "Dairy"
+                    },
+                    new Models.Product
+                    {
+                        Name = "Banana Bean Bliss",
+                        Price = 9.99m,
+                        Category = "Dairy - Ice Cream"
+                    },
+                    new Models.Product
+                    {
+                        Name = "Farmhouse Vanilla",
+                        Price = 9.99m,
+                        Category = "Dairy - Ice Cream"
+                    },
+                    new Models.Product
+                    {
+                        Name = "Martin Cow Plush",
+                        Price = 19.99m,
+                        Category = "Merch"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- eCommerce/Models/Product.cs
+++ eCommerce/Models/Product.cs
@@ -1,4 +1,7 @@
-﻿namespace eCommerce.Models
+﻿using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.Models
 {
 
     /// <summary>
@@ -8,10 +11,35 @@
     /// </summary>
     public class Product
     {
+        [Key]
         public int ProductId { get; set; }
+        /// <summary>
+        /// This gets and or sets the name of the product. 
+        /// This is a required field and must be filled out when creating a new product.
+        /// </summary>
         public required string Name { get; set; }
+        /// <summary>
+        /// This gets and or sets the price of the product. This is a required field and must be filled out when creating a new product. The price is represented as a decimal value to allow for cents and dollars. 
+        /// The price should be a positive value and should not exceed a reasonable amount for an eCommerce product.
+        /// </summary>
         public decimal Price { get; set; }
+        /// <summary>
+        /// This gets and or sets the category of the product. This is a required field and must be filled 
+        /// out when creating a new product. 
+        /// The category helps to organize products into different groups, making it easier for customers 
+        /// to find what they are looking for. Examples of categories could include "Dairy",
+        /// "Merch", etc.
+        /// </summary>
         public required string Category { get; set; }
-
+        /// <summary>
+        /// This gets and or sets the description of the product. This is an optional field that can be filled out when creating a new product. The description provides additional information about the product, such as its features, benefits, or any other relevant details that may help customers make informed purchasing decisions. The description can be a string of text that gives customers a better 
+        /// understanding of what the product is and why they might want to buy it.
+        /// </summary>
+        public string? Description { get; set; }
+        /// <summary>
+        /// This gets and or sets the image URL of the product. This is an optional field that can be filled out when creating a new product. The image URL is a string that represents the location of an image file that visually represents the product. This can be a link to an image hosted on a server or a local file path. The image helps customers to see what the product looks like, 
+        /// which can enhance their shopping experience and increase the likelihood of making a purchase.
+        /// </summary>
+        public string? ImageUrl { get; set; }
     }
 }
