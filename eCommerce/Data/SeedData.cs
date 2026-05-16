8e77f85d56943a62ef7ab250940d298fdfd6f248
VacaSama <klc_moore@outlook.com>
2026-05-16 16:38:27 -0400

    The database was dropped and reset.
    SeedData has also been updated, with images, descriptions and prices-- Using Entity Framework instead of a premade database for flexibility. The Original Whole milk .png was changed so some other files needs to be changed in order for image visibility to work.

--- eCommerce/Data/SeedData.cs
+++ eCommerce/Data/SeedData.cs
@@ -19,37 +19,55 @@ namespace eCommerce.Data
                     {
                         Name = "Original Milkman",
                         Price = 7.99m,
-                        Category = "Dairy"
+                        Category = "Dairy", 
+                        Description = "The Original Milkman is a classic, creamy beverage that has been a favorite for generations. " +
+                        "Made with the finest ingredients, this delicious dairy product offers a rich and smooth flavor that will transport" +
+                        " you back to simpler times." +
+                        "The Original Milkman is sure to satisfy your cravings and bring a smile to your face.",
+                        ImageUrl = "MartinOriginal.png"
                     },
                     new Models.Product
                     {
                         Name = "Little Chubbs Chocolate Delight",
                         Price = 7.99m,
-                        Category = "Dairy"
+                        Category = "Dairy", 
+                        Description = "Indulge in the irresistible sweetness of Little Chubbs Chocolate Delight, a delectable treat that will satisfy your chocolate cravings. ",
+                        ImageUrl = "ChocolateMartin.png"
                     },
                     new Models.Product
                     {
                         Name = "Aunt May Berry Pie",
                         Price = 8.99m,
-                        Category = "Dairy"
+                        Category = "Dairy",
+                        Description = "Experience the delightful taste of Aunt May Berry Pie, our limited edition milk flavor" +
+                        " a scrumptious beverage that combines the sweetness of ripe strawberries with a buttery, flaky, graham cracker crust.",
+                        ImageUrl = "BerryPieMartin.png"
                     },
                     new Models.Product
                     {
                         Name = "Banana Bean Bliss",
                         Price = 9.99m,
-                        Category = "Dairy - Ice Cream"
+                        Category = "Dairy - Ice Cream",
+                        Description = "Banana Bean Bliss is a heavenly blend of creamy banana and rich java bean flavors, " +
+                        "creating a delightful treat that will transport your taste buds to a caffinated paradise.",
+                        ImageUrl = "Milkman-BJavaChip.png"
                     },
                     new Models.Product
                     {
                         Name = "Farmhouse Vanilla",
                         Price = 9.99m,
-                        Category = "Dairy - Ice Cream"
+                        Category = "Dairy - Ice Cream", 
+                        Description = "Farmhouse Vanilla is a timeless classic that captures the essence of creamy, rich vanilla bean flavor.",
+                        ImageUrl = "Milkman-VanillaBean.png"
                     },
                     new Models.Product
                     {
                         Name = "Martin Cow Plush",
                         Price = 19.99m,
-                        Category = "Merch"
+                        Category = "Merch",
+                        Description = "Bring home the charm of our mascot, Martin Cow! The cutest dairy deliverer in the game, " +
+                        "all purchases of the Martin Cow Plush will help local dairy farms deliver fresh milk to participating schools.",
+                        ImageUrl = "MilkmanPlush.png"
                     }
                 );
                 context.SaveChanges();
