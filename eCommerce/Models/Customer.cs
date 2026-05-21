9737ee157597193846cb0b83314d81e354303293
VacaSama <klc_moore@outlook.com>
2026-05-21 15:55:31 -0400

    * CustomerController-DELETED * Customer Folder -> Manual Login, Register DELETED Customer Model is being cleaned up now that Identity has been scaffolded into the application. Email - REMOVED. RegistrationViewModel - REMOVED. LoginViewModel - REMOVED. ZipCode - ADDED (ZipCode is a string so logic will need to be implemented to ensure that only digits are submitted).

--- eCommerce/Models/Customer.cs
+++ eCommerce/Models/Customer.cs
@@ -10,6 +10,7 @@ public class Customer
     /// </summary>
     public int CustomerId { get; set; }
 
+    // Customer Personal Information BELOW
     /// <summary>
     /// 
     /// </summary>
@@ -20,30 +21,15 @@ public class Customer
     /// </summary>
     public required string LastName { get; set; }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    [DataType(DataType.EmailAddress)]
-    public required string Email { get; set; }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// 
-    [StringLength(50, MinimumLength = 8)]
-
-    [DataType(DataType.Password)]
-    public required string Password { get; set; }
-
+    // Customer Billing Information BELOW
     /// <summary>
     /// 
     /// </summary>
     public string? Phone { get; set; }
-
+    
     /// <summary>
     /// 
     /// </summary>
-
     public required string StreetAddress { get; set; } 
 
     /// <summary>
@@ -56,57 +42,12 @@ public class Customer
     /// 
     /// </summary>
     public required string State { get; set; }
-}
-
-
-/// <summary>
-/// A class within a class, brilliant. It is used as a ViewModel for registration.
-/// </summary>
-public class RegistrationViewModel
-{
-
-    public required string FirstName { get; set; }
-
-    public required string LastName { get; set; }
-
-
-    [DataType(DataType.EmailAddress)]
-    public required string Email { get; set; }
-
-
-    [StringLength(50, MinimumLength = 8)]
-
-    [DataType(DataType.Password)]
-    public required string Password { get; set; }
 
     /// <summary>
     /// 
     /// </summary>
-    [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
-    [DataType(DataType.Password)]
-    public required string ConfirmPassword { get; set; }
-
-    public string? Phone { get; set; }
-
-    public required string StreetAddress { get; set; }
-
-    public required string City { get; set; }
-
-    public required string State { get; set; }
+    public required string ZipCode { get; set; }
 }
 
-/// <summary>
-/// LoginViewmodel is another class within a class that is related to Customers class
-/// so it will be stored here. Yes, you can do that. It is used as a ViewModel for login.
-/// </summary>
-public class LoginViewModel
-{
-
-    [DataType(DataType.EmailAddress)]
-    public required string  Email { get; set;}
 
 
-    [DataType(DataType.Password)]
-    public required string  Password {get; set;} 
-}
-  
