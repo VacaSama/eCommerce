0913e5fc01b4c0ae29bbfea0af62f36d8505d90f
VacaSama <klc_moore@outlook.com>
2026-05-21 16:04:09 -0400

    Summaries have been added to the Customer model for each property.

--- eCommerce/Models/Customer.cs
+++ eCommerce/Models/Customer.cs
@@ -6,45 +6,48 @@ namespace eCommerce.Models;
 public class Customer
 {
     /// <summary>
-    /// 
+    /// Unique Identifier for the Customer Model 
     /// </summary>
     public int CustomerId { get; set; }
 
+    // Link to IdentityUser BELOW
+    public string? IdentityUserId { get; set; }
+    public IdentityUser? IdentityUser { get; set; }
+
     // Customer Personal Information BELOW
     /// <summary>
-    /// 
+    /// Gets or sets the first name of the customer. This is a required field and cannot be null or empty.
     /// </summary>
     public required string FirstName { get; set; }
 
     /// <summary>
-    /// 
+    /// Gets or sets the last name of the customer. This is a required field and cannot be null or empty.
     /// </summary>
     public required string LastName { get; set; }
 
     // Customer Billing Information BELOW
     /// <summary>
-    /// 
+    /// Gets or sets the phone number of the customer. This is an optional field and must be a valid email format.
     /// </summary>
     public string? Phone { get; set; }
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    public required string StreetAddress { get; set; } 
 
     /// <summary>
-    /// 
+    /// Gets or sets the Street Address of the customer. This is a required field and cannot be null or empty.
     /// </summary>
+    public required string StreetAddress { get; set; }
 
+    /// <summary>
+    /// Gets or sets the city of the customer. This is a required field and cannot be null or empty.
+    /// </summary>
     public required string City { get; set; }
 
     /// <summary>
-    /// 
+    /// Gets or sets the state of the customer. This is a required field and cannot be null or empty.
     /// </summary>
     public required string State { get; set; }
 
     /// <summary>
-    /// 
+    /// Gets or sets the zip code of the customer. This is a required field and cannot be null or empty.
     /// </summary>
     public required string ZipCode { get; set; }
 }
